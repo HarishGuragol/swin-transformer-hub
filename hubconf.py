e021e729ff6bb83bbd0ff005349ce0060a1cead8
Harish Guragol <42379451+HarishGuragol@users.noreply.github.com>
2023-04-27 01:09:45 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -5,7 +5,8 @@ import swin
 
 MODEL_URLS = {
     "swin_tiny_patch4_window7_224": "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth",
-    "swin_base_patch4_window7_224": "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth"
+    "swin_base_patch4_window7_224": "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth",
+    "swinv2_base_patch4_window8_256": "https://github.com/SwinTransformer/storage/releases/download/v2.0.0/swinv2_base_patch4_window8_256.pth
 }
 
 def swin_tiny_patch4_window7_224(pretrained=False):
